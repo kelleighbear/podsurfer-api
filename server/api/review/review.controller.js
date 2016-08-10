e6557ddfd69185be4cefdfc24822e62461a0f98f
Kelleigh Maroney <kmaroney@credera.com>
2016-08-10 16:17:36 -0500

    Added tests on Reviews

--- server/api/review/review.controller.js
+++ server/api/review/review.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Review = require('./review.model');
+const mongoose = require('mongoose');
 const _ = require('lodash');
 
 module.exports = {
@@ -30,7 +31,7 @@ function handleError(res, statusCode) {
  */
 function getMine(req, res) {
     return Review.find({
-            reviewer: req.user._id
+            'reviewer.id': req.user._id
         }).exec()
         .then(reviews => {
             res.status(200).json(reviews);
@@ -41,6 +42,9 @@ function getMine(req, res) {
  * Get one podcast
  */
 function getForPodcast(req, res) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(500).send('Not a valid podcast ID');
+    }
     return Review.find({
             podcast: req.params.id
         }).exec()
@@ -53,26 +57,26 @@ function getForPodcast(req, res) {
  * Create new podcast
  */
 function create(req, res) {
-    if(!req.body.podcast || !req.body.review || !req.body.rating) {
-      return res.status(500).send('Review must include podcast id, rating, and text.');
+    if (!req.body.podcast || !req.body.review || !req.body.rating) {
+        return res.status(500).send('Review must include podcast id, rating, and text.');
     }
     var newReview = new Review(req.body);
     newReview.reviewer = {
-      id: req.user._id,
-      name: req.user.name
+        id: req.user._id,
+        name: req.user.name
     };
-    Review.find({
-            reviewer: newReview.reviewer,
+    return Review.find({
+            'reviewer.id': newReview.reviewer.id,
             podcast: newReview.podcast,
             episode: newReview.episode
         }).exec()
         .then(function(reviews) {
-            if (reviews) {
+            if (reviews.length > 0) {
                 return res.status(500).send('You already have a review for this podcast/episode!');
             } else {
                 return newReview.save()
-                    .then(function(podcast) {
-                        res.status(200).json(podcast);
+                    .then(review => {
+                        res.status(200).json(review);
                     })
                     .catch(validationError(res));
             }
@@ -83,12 +87,15 @@ function create(req, res) {
  * Update a podcast
  */
 function update(req, res, next) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(500).send('Not a valid podcast ID');
+    }
     return Review.findById({
             _id: req.params.id
         }).exec()
         .then(review => {
             if (review !== null) {
-                if (review.reviewer.equals(req.user._id)) {
+                if (req.user._id.equals(review.reviewer.id)) {
                     review = _.merge(review, req.body);
                     return review.save({
                             new: true
@@ -98,7 +105,6 @@ function update(req, res, next) {
                 } else {
                     return res.status(500).send('This is not your review - you cannot change it.');
                 }
-
             } else {
                 next('Podcast was null');
             }
@@ -109,9 +115,12 @@ function update(req, res, next) {
  * Permanently remove a podcast
  */
 function destroy(req, res) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(500).send('Not a valid podcast ID');
+    }
     return Review.findById(req.params.id).exec()
         .then(review => {
-            if (review.reviewer.equals(req.user._id)) {
+            if (req.user._id.equals(review.reviewer.id)) {
                 return Review.findByIdAndRemove(req.params.id).exec()
                     .then(function() {
                         res.status(204).end();
--- server/api/review/review.controller.spec.js
+++ server/api/review/review.controller.spec.js
@@ -10,136 +10,305 @@ const expect = require('chai').expect;
 const _ = require('lodash');
 
 describe('Review Controller', function() {
-  let user;
+    let user, podcast1, review1, review2;
 
-  // Clear users before testing
-  before(function() {
-    return User.remove()
-    .then(() => User.create({
-          name: 'Fake User',
-          email: 'test@example.com',
-          password: 'password0W#'
-      }))
-      .then(newUser => {
-        user = newUser;
-        Promise.resolve();
+    // Clear users before testing
+    before(function() {
+        return User.remove()
+            .then(() => User.create({
+                name: 'Fake User',
+                email: 'test@example.com',
+                password: 'password0W#'
+            }))
+            .then(newUser => {
+                user = newUser;
+                Promise.resolve();
+            })
+            .then(() => Podcast.remove())
+            .then(() => Podcast.create({
+                name: 'TylerPodcast',
+                description: 'A thrilling podcast detailing the life of Tyler Estes.'
+            }))
+            .then(newPodcast => {
+                podcast1 = newPodcast;
+                Promise.resolve();
+            })
+            .then(() => Review.create({
+                podcast: podcast1._id,
+                review: 'A thrilling podcast detailing the life of Tyler Estes... Best thing I\'ve ever listened to in my life',
+                rating: 4.5
+            }))
+            .then(newReview => {
+                review1 = newReview;
+                Promise.resolve();
+            });
+    });
+
+    // Clear users after testing
+    after(function() {
+        return User.remove();
+    });
+
+    // Clear podcasts after testing
+    after(function() {
+        return Podcast.remove();
+    });
+
+    // Clear reviews after testing
+    after(function() {
+        return Review.remove();
+    });
+
+    describe('POST /api/podcast/', function() {
+        var token;
+
+        before(function(done) {
+            request(app)
+                .post('/auth/local')
+                .send({
+                    email: 'test@example.com',
+                    password: 'password0W#'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    expect(err).to.not.exist;
+                    token = res.body.token;
+                    done();
+                });
+        });
+
+        it('should fail to create a review with no podcast ID', function(done) {
+            let review = {};
+            request(app)
+                .post('/api/review')
+                .set('authorization', 'Bearer ' + token)
+                .send(review)
+                .expect(500, done);
+        });
+
+        it('should fail to create a review with no review text', function(done) {
+            let review = {
+                podcast: podcast1._id
+            };
+            request(app)
+                .post('/api/review')
+                .set('authorization', 'Bearer ' + token)
+                .send(review)
+                .expect(500, done);
+        });
+
+        it('should fail to create a review with no rating', function(done) {
+            let review = {
+                podcast: podcast1._id,
+                review: 'omg, amazing'
+            };
+            request(app)
+                .post('/api/review')
+                .set('authorization', 'Bearer ' + token)
+                .send(review)
+                .expect(500, done);
+        });
+
+        it('should create a valid review', function(done) {
+            let review = {
+                podcast: podcast1._id,
+                review: 'omg, amazing',
+                rating: 4
+            };
+            request(app)
+                .post('/api/review')
+                .set('authorization', 'Bearer ' + token)
+                .send(review)
+                .expect(200)
+                .end((err, res) => {
+                    review2 = res.body;
+                    expect(review2.review).to.equal('omg, amazing');
+                    expect(review2.rating).to.equal(4);
+                    done();
+                });
+        });
+
+        it('should fail to create a review by the same user for the same podcast', function(done) {
+            let review = {
+                podcast: podcast1._id,
+                review: 'omg, hated it',
+                rating: 3
+            };
+            request(app)
+                .post('/api/review')
+                .set('authorization', 'Bearer ' + token)
+                .send(review)
+                .expect(500, done);
+        });
+    });
+
+    describe('GET /api/podcast/:id', function() {
+        it('should fail to get reviews for a podcast with invalid ID', function(done) {
+            request(app)
+                .get('/api/review/12345')
+                .expect(500, done);
+        });
+
+        it('should fail to get reviews for a podcast with no ID', function(done) {
+            request(app)
+                .get('/api/review/')
+                .expect(404, done);
+        });
+
+        it('should get all reviews for a podcast', function(done) {
+            request(app)
+                .get('/api/review/' + podcast1._id)
+                .expect(200)
+                .end((err, res) => {
+                    let result = res.body;
+                    expect(result.length).to.equal(2);
+                    done();
+                });
+        });
+    });
+
+    describe('GET /api/review/mine', function() {
+      var token;
+
+      before(function(done) {
+          request(app)
+              .post('/auth/local')
+              .send({
+                  email: 'test@example.com',
+                  password: 'password0W#'
+              })
+              .expect(200)
+              .expect('Content-Type', /json/)
+              .end((err, res) => {
+                  expect(err).to.not.exist;
+                  token = res.body.token;
+                  done();
+              });
       });
-  });
 
-  // Clear users after testing
-  after(function() {
-    return User.remove();
-  });
+      it('should get all my reviews', function(done) {
+          request(app)
+              .get('/api/review/mine')
+              .set('authorization', 'Bearer ' + token)
+              .expect(200)
+              .end((err, res) => {
+                  let result = res.body;
+                  expect(result.length).to.equal(1);
+                  done();
+              });
+      });
+    });
 
-  // describe('POST /api/review', function() {
-  //   it('should fail to create a review without a podcast ID', function(done) {
-  //     let user2 = new User({
-  //       name: 'Fake User2',
-  //       email: 'test@example2.com',
-  //       password: 'badpass'
-  //     });
-  //     request(app)
-  //       .post('/api/user')
-  //       .send(user2)
-  //       .expect(422, done);
-  //   });
-  // });
-  //
-  // describe('POST /api/user', function() {
-  //   it('should fail to create a review without review text', function(done) {
-  //     let user2 = new User({
-  //       name: 'Fake User2',
-  //       email: 'test',
-  //       password: 'password0W#'
-  //     });
-  //     request(app)
-  //       .post('/api/user')
-  //       .send(user2)
-  //       .expect(422, done);
-  //   });
-  // });
-  //
-  // describe('POST /api/user', function() {
-  //   it('should fail to create a review without a rating', function(done) {
-  //     let user2 = new User({
-  //       email: 'test@test.com',
-  //       password: 'password0W#'
-  //     });
-  //     request(app)
-  //       .post('/api/user')
-  //       .send(user2)
-  //       .expect(422, done);
-  //   });
-  // });
-  //
-  // describe('POST /api/user', function() {
-  //   it('should create a valid review', function(done) {
-  //     let user2 = new User({
-  //       name: 'Kelleigh Laine',
-  //       email: 'kmaroney@credera.com',
-  //       password: 'password0W#'
-  //     });
-  //     request(app)
-  //       .post('/api/user')
-  //       .send(user2)
-  //       .expect(200, done);
-  //   });
-  // });
-  //
-  // describe('PUT /api/review/:id', function() {
-  //   var token;
-  //
-  //   before(function(done) {
-  //     request(app)
-  //       .post('/auth/local')
-  //       .send({
-  //         email: 'test@example.com',
-  //         password: 'password0W#'
-  //       })
-  //       .expect(200)
-  //       .expect('Content-Type', /json/)
-  //       .end((err, res) => {
-  //         expect(err).to.not.exist;
-  //         token = res.body.token;
-  //         done();
-  //       });
-  //   });
-  //
-  //   it('should fail to update a review that belongs to another user', function(done) {
-  //     let updatedUser = _.merge(user, {
-  //         interests: ['technology']
-  //       });
-  //     request(app)
-  //       .put('/api/review/')
-  //       .set('authorization', 'Bearer ' + token)
-  //       .send(updatedUser)
-  //       .expect(200)
-  //       .end((err, res) => {
-  //         expect(err).to.not.exist;
-  //         let result = res.body;
-  //         expect(result.interests.length).to.equal(1);
-  //         expect(result.name).to.equal('Fake User');
-  //         done();
-  //       });
-  //   });
-  //
-  //   it('should update a review', function(done) {
-  //     let updatedUser = _.merge(user, {
-  //         interests: ['technology']
-  //       });
-  //     request(app)
-  //       .put('/api/review/')
-  //       .set('authorization', 'Bearer ' + token)
-  //       .send(updatedUser)
-  //       .expect(200)
-  //       .end((err, res) => {
-  //         expect(err).to.not.exist;
-  //         let result = res.body;
-  //         expect(result.interests.length).to.equal(1);
-  //         expect(result.name).to.equal('Fake User');
-  //         done();
-  //       });
-  //   });
-  // });
+    describe('PUT /api/review/:id', function() {
+        var token;
+
+        before(function(done) {
+            request(app)
+                .post('/auth/local')
+                .send({
+                    email: 'test@example.com',
+                    password: 'password0W#'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    expect(err).to.not.exist;
+                    token = res.body.token;
+                    done();
+                });
+        });
+
+        it('should fail to update a review with invalid ID', function(done) {
+            request(app)
+                .put('/api/review/12345')
+                .set('authorization', 'Bearer ' + token)
+                .expect(500, done);
+        });
+
+        it('should fail to update a review with no ID', function(done) {
+            request(app)
+                .put('/api/review/')
+                .set('authorization', 'Bearer ' + token)
+                .expect(404, done);
+        });
+
+        it('should fail to update a review you did not write', function(done) {
+            var updates = {
+                episode: 1
+            };
+            request(app)
+                .put('/api/review/' + review1._id)
+                .set('authorization', 'Bearer ' + token)
+                .send(updates)
+                .expect(500, done);
+        });
+
+        it('should update a review', function(done) {
+            let updates = {
+              episode: 1
+            };
+            request(app)
+                .put('/api/review/' + review2._id)
+                .set('authorization', 'Bearer ' + token)
+                .send(updates)
+                .expect(200)
+                .end((err, res) => {
+                    let result = res.body;
+                    expect(result.episode).to.equal(1);
+                    expect(result.review).to.equal('omg, amazing');
+                    expect(result.rating).to.equal(4);
+                    done();
+                });
+        });
+    });
+
+    describe('DELETE /api/review/:id', function() {
+      var token;
+
+        before(function(done) {
+            request(app)
+                .post('/auth/local')
+                .send({
+                    email: 'test@example.com',
+                    password: 'password0W#'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    expect(err).to.not.exist;
+                    token = res.body.token;
+                    done();
+                });
+        });
+
+        it('should fail to delete a review with invalid ID', function(done) {
+            request(app)
+                .delete('/api/review/12345')
+                .set('authorization', 'Bearer ' + token)
+                .expect(500, done);
+        });
+
+        it('should fail to delete a review with no ID', function(done) {
+            request(app)
+                .delete('/api/review/')
+                .set('authorization', 'Bearer ' + token)
+                .expect(404, done);
+        });
+
+        it('should fail to delete a review that you did not write', function(done) {
+            request(app)
+                .delete('/api/review/' + review1._id)
+                .set('authorization', 'Bearer ' + token)
+                .expect(500, done);
+        });
+
+        it('should successfully delete a review', function(done) {
+            request(app)
+                .delete('/api/review/' + review2._id)
+                .set('authorization', 'Bearer ' + token)
+                .expect(204, done);
+        });
+    });
 
 });
